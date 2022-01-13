b267f21a0023b71f81f43e6a37889125a15e1d19
unknown <lazarodaniel80@gmail.com>
2022-01-12 21:33:22 -0300

    Feature/alga-01 criações dos métodos POST e GET, e chamadas vias POSTMAN

---
+++ ReposAlgaWorks/alga-api/src/test/java/com/alga/api/AlgaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alga.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
