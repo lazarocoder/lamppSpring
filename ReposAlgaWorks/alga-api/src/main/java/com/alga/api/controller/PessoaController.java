2090c1fcd13543f508b83a8d1d863829e43623c9
unknown <lazarodaniel80@gmail.com>
2022-02-09 21:52:49 -0300

    Feature/alga criando Listener e Event para pessoa

--- ReposAlgaWorks/alga-api/src/main/java/com/alga/api/controller/PessoaController.java
+++ ReposAlgaWorks/alga-api/src/main/java/com/alga/api/controller/PessoaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alga.api.event.EventController;
 import com.alga.api.model.Pessoa;
 import com.alga.api.repository.PessoaRepository;
 
@@ -23,7 +25,9 @@ public class PessoaController {
 
 	@Autowired
 	PessoaRepository pessoaRepository;
-
+	
+	public ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Pessoa> buscarPessoas() {
 		return pessoaRepository.findAll();
@@ -33,6 +37,7 @@ public class PessoaController {
 	@PostMapping
 	public ResponseEntity<Pessoa> salvarPessoa(@Validated @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		publisher.publishEvent(new EventController(this, response, pessoaSalva.getCodigo()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 
 	}
